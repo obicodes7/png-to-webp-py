1f778ed6bef0566f813cff0e706b02f00785393d
Fadhli S <39572912+obicodes7@users.noreply.github.com>
2025-06-17 09:50:39 +0700

    delete if converted

--- pngtowebp.py
+++ pngtowebp.py
@@ -8,10 +8,12 @@ def list_files_recursive(path='.'):
         if os.path.isdir(full_path):
             list_files_recursive(full_path)
         else:
-            if "pngtowebp.py" not in full_path:
-                output_webp = full_path.replace('.png', '.webp')
-                convert_png_to_webp(full_path, output_webp)
-                os.remove(full_path)
+            if ".jpg" in full_path.lower() or ".png" in full_path.lower() or ".jepg" in full_path.lower():
+                output_webp = full_path.replace('.png', '.webp').replace(
+                    '.jpg', '.webp').replace('.jpeg', '.webp')
+                result = convert_png_to_webp(full_path, output_webp)
+                if result:
+                    os.remove(full_path)
 
 
 def convert_png_to_webp(input_path, output_path):
@@ -20,10 +22,13 @@ def convert_png_to_webp(input_path, output_path):
         image = image.convert('RGB')  # Convert to RGB if needed
         image.save(output_path, 'webp')
         print(f"Converted {input_path} to {output_path}")
+        return True
     except FileNotFoundError:
         print(f"Error: Input file not found at {input_path}")
+        return False
     except Exception as e:
         print(f"An error occurred: {e}")
+        return False
 
 
 directory_path = './'
